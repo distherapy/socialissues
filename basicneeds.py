fbf15f6e473fc68336ebcffd66c4badd562e1992
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2022-08-20 17:05:11 -0400

    Create basicneeds.py

---
+++ basicneeds.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+a = #num_people
+ = #num_calories(to exist healthily, not just survive)
+ = #num_gal_water(to exist healthily[including washing, not just survive)
+ = #num_res_properties
+ = #num_acres_frootable_land_am
+ = #num_acres_frootable_land_eu
+ = #num_acres_frootable_land_af
+ = #num_acres_frootable_land_as
+ = #num_acres_frootable_land_sa
+ = #num_acres_frootable_land_au
+
+
+def ag():
+
+def growth_humans():
+	
+def growth_other_species():
