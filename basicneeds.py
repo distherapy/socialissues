2f7bf5d2d99ccd2e6224ff163eda26abae4e4b7e
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2022-08-21 15:02:45 -0400

    Update basicneeds.py

--- basicneeds.py
+++ basicneeds.py
@@ -1,24 +1,37 @@
 #!/usr/bin/env python
 
-a = #num_people
- = #num_calories(to exist healthily, not just survive)
- = #num_gal_water(to exist healthily[including washing, not just survive)
- = #num_res_properties
- = #num_acres_frootable_land_am
- = #num_acres_frootable_land_eu
- = #num_acres_frootable_land_af
- = #num_acres_frootable_land_as
- = #num_acres_frootable_land_sa
- = #num_acres_frootable_land_au
+'''algorithms to solve these social issues:
+water,
+food,
+shelter,
+'''
 
-
+def water():
+	num_gal = #num_gal water per day per person(to exist healthily[including washing, not just survive)
+	
 def ag():
-	w = num_livestock
-	x = edible_food_fed_to_livestock
-	y = 
-
+	pig = #num_pigs
+	beef = #num_cows
+	hens = #num_chickens
+	ls = #num_livestock
+	fed2ls = #edible_food_fed_to_livestock
+	num_ac_am = #num_acres_frootable_land_am
+	num_ac_eu = #num_acres_frootable_land_eu
+	num_ac_af = #num_acres_frootable_land_af
+	num_ac_as = #num_acres_frootable_land_as
+	num_ac_sa = #num_acres_frootable_land_sa
+	num_ac_au = #num_acres_frootable_land_au
+	num_cal_req = #num_calories(to exist healthily, not just survive)
 def growth_humans():
 	num_births - num_deaths
+	num_hum = #num_people
+	num_fam = #num_familial_units
+	num_hum_am = #num_people_am
+	num_hum_eu = #num_people_eu
+	num_hum_af = #num_people_af
+	num_hum_as = #num_people_as
+	num_hum_sa = #num_people_sa
+	num_hum_au = #num_people_au
 	
 def growth_other_species():
 	pass
@@ -26,9 +39,8 @@ def growth_other_species():
 def housing():
 	comme = #num_commercial_buildings(empty)
 	commo = #num_commercial_buildings(occupied)
-	rese =
-	reso = 
+	rese = #num_residential_buildings(empty)
+	reso = #num_residential_buildings(occupied)
 
 def waste():
 	pass
-
