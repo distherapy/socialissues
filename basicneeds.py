0c54e48d4d4bfb3269e8332bda405949f56eeb66
Brent Elisens <56242810+distherapy@users.noreply.github.com>
2022-08-22 05:01:27 -0400

    Update basicneeds.py

--- basicneeds.py
+++ basicneeds.py
@@ -10,11 +10,18 @@ def water():
 	num_gal = #num_gal water per day per person(to exist healthily[including washing, not just survive)
 	
 def ag():
+	'''
+	'farming' animals is a problem that can and SHOULD be avoided.
+	if their suffering doesn't sway your pampered, entitled cheeks,
+	then the exponential use and waste of food and water should.
+	'''
+
 	pig = #num_pigs
 	beef = #num_cows
 	hens = #num_chickens
 	ls = #num_livestock
 	fed2ls = #edible_food_fed_to_livestock
+	water2ls = #potable_water_used_for_ag
 	num_ac_am = #num_acres_frootable_land_am
 	num_ac_eu = #num_acres_frootable_land_eu
 	num_ac_af = #num_acres_frootable_land_af
@@ -22,6 +29,7 @@ def ag():
 	num_ac_sa = #num_acres_frootable_land_sa
 	num_ac_au = #num_acres_frootable_land_au
 	num_cal_req = #num_calories(to exist healthily, not just survive)
+	
 def growth_humans():
 	num_births - num_deaths
 	num_hum = #num_people
@@ -44,3 +52,10 @@ def housing():
 
 def waste():
 	pass
+
+def for_user():
+	cnt = input("Continent:")
+	tmp = int(input("Current average temperature:")
+	sol = int(input("Current average hours sun per day:")
+	rpd = int(input("Current average rainfall per day:")
+	gnd = input("Soil type:")
